6cd434d9c3ad70568f2c4424bf68f522d2109919
Shadowborn <james.starks023@gmail.com>
2019-04-16 09:55:15 -0600

    Navigation fade on scroll

--- js/index2.js
+++ js/index2.js
@@ -5,3 +5,12 @@ logoRefresh.addEventListener('click', () => {
         window.location.reload(true);
     }, 200);
 });
+
+
+
+$(document).on('scroll', function (e) {
+    if ($(window).width() > 992) {
+		var rgba = $(document).scrollTop() / 500;
+   $('.main-navigation').css('background-color', 'rgba(255,255,255,' + rgba + ')');
+	}
+})
