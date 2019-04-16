fdbe19b677649de72f3baa64b0ed3c684061f28a
Shadowborn <james.starks023@gmail.com>
2019-04-16 14:31:54 -0600

    Forgot to commit all day. Updates to main page and about us

--- js/index2.js
+++ js/index2.js
@@ -11,6 +11,6 @@ logoRefresh.addEventListener('click', () => {
 $(document).on('scroll', function (e) {
     if ($(window).width() > 992) {
 		var rgba = $(document).scrollTop() / 500;
-   $('.main-navigation').css('background-color', 'rgba(255,255,255,' + rgba + ')');
+   $('.main-navigation').css('background-color', 'rgba(73, 101, 123,' + rgba + ')');
 	}
 })
